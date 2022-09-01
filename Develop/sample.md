a903717c42bd9198ca7a54f0473d5072eac36c0f
SSPENELOPE <typoepping@gmail.com>
2022-09-01 13:30:20 -0400

    added return data in genertateMarkdown file

--- Develop/sample.md
+++ Develop/sample.md
@@ -1,2 +1,15 @@
-# A NEW PROJECT
-  
+# A New Project 
+  ## Description
+  - adkjf;asldkjf;asldkfjasd;lfk
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Credits](#credits)
+  - [License](#License)
+  ## Installation
+  - undefined
+  ## Usage 
+  - asdfasdfasdfasdfasdwf
+  ## License
+  - The MIT License 
+
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -18,8 +18,21 @@ function renderLicenseSection(license) {
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-  
+  return `# ${data.title} 
+  ## Description
+  - ${data.description}
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Credits](#credits)
+  - [License](#License)
+  ## Installation
+  - ${data.intallation}
+  ## Usage 
+  - ${data.usage}
+  ## License
+  - ${data.license} 
+
 `;
 }
 

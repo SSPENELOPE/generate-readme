75586fd8aaa5cf08a24a7bddc7b8ec491555f58a
SSPENELOPE <typoepping@gmail.com>
2022-09-08 13:21:20 -0400

    changed license section to match project requirements

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -19,13 +19,13 @@ function renderLicenseBadge(license) {
 function renderLicenseLink(license) {
   let userLicenseLink = '';
   if (license.license === 'The MIT License') {
-    userLicenseLink = ``
+    userLicenseLink = `(https://opensource.org/licenses/MIT)`
   } else if(license.license === 'The GPL License'){
     userLicenseLink = `(https://www.gnu.org/licenses/gpl-3.0)`
   } else if (license.license === 'Apache 2.0') {
     userLicenseLink = `(https://opensource.org/licenses/Apache-2.0)`
   } else {
-    userLicenseLink = `(https://opensource.org/licenses/MIT)`;
+    userLicenseLink = ``;
   }
     return userLicenseLink;
 }
@@ -33,13 +33,24 @@ function renderLicenseLink(license) {
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-  let licenseSection = renderLicenseBadge(license) + renderLicenseLink(license);
+  let licenseSection = '';
+  if (license.license === 'The MIT License') {
+    licenseSection = `This repository is licensed under 'The MIT License'`
+  } else if(license.license === 'The GPL License'){
+    licenseSection = `This repository is licensed under 'The GPL License'`
+  } else if (license.license === 'Apache 2.0') {
+    licenseSection = `This repository is licensed under 'The Apache 2.0 License'`
+  } else {
+    licenseSection = ``;
+  }
   return licenseSection;
 }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `# ${renderLicenseBadge(data)}
+  
+  # ${data.title}
 
   ## Description
   - ${data.description}
@@ -60,7 +71,8 @@ function generateMarkdown(data) {
   - ${data.credits}
 
   ## License
-  - ${renderLicenseSection(data)}
+  - ${renderLicenseSection(data)} 
+  - ${renderLicenseLink(data)}
 
   ## Contributing
   - ${data.contribute}
